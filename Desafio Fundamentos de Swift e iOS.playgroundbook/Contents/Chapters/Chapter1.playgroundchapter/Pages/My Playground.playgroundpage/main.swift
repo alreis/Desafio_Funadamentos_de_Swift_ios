0829ca64df054f4b8be3a2996a0430553efcb7d6
alreis <al.reis@live.com>
2023-09-08 21:23:20 -0300

    third commit

--- Desafio Fundamentos de Swift e iOS.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/My Playground.playgroundpage/main.swift
+++ Desafio Fundamentos de Swift e iOS.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/My Playground.playgroundpage/main.swift
@@ -1,1 +1,21 @@
 
+let firstName: String = "Steve"
+var secondName: String? = "Jobs"
+
+// Usando interpolação de strings com um valor padrão para a variável opcional
+if let secondName = secondName {
+    print("O nome completo é \(firstName) \(secondName)")
+} else {
+    secondName = "Wosniak"
+    print("O nome completo é \(firstName) \(secondName)")
+}
+
+
+// Optional Binding para desembrulhar a variável opcional
+if let unwrappedSecondName = secondName {
+    print("O nome completo é \(firstName) \(unwrappedSecondName)")
+} else {
+    secondName = "Wosniak"
+    print("O nome completo é \(firstName) \(secondName)")
+}
+
